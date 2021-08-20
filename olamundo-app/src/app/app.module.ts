6436f704f5ccc4c0a07bfdec07caa83e7baf0343
Victor Vidigal Ribeiro <victorvidigal@gmail.com>
2021-08-19 22:07:21 -0300

    primeiro commit

---
+++ olamundo-app/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TopoComponent } from './topo/topo.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
+import {MatCardModule} from "@angular/material/card";
+import {MatFormFieldModule} from "@angular/material/form-field";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TopoComponent,
+    RodapeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent, TopoComponent, RodapeComponent]
+})
+export class AppModule { }
